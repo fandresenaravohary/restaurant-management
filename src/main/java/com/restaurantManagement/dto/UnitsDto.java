fbb2b05c37e4cb04b07116e3fc18c34eb1c18708
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-04-06 21:00:40 +0300

    refactor: add jakarta validation in UnitsDto

--- src/main/java/com/restaurantManagement/dto/UnitsDto.java
+++ src/main/java/com/restaurantManagement/dto/UnitsDto.java
@@ -1,5 +1,7 @@
 package com.restaurantManagement.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,12 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UnitsDto {
     private Long id;
+
+    @NotNull(message = "Le nom de l'unité ne peut pas être nul")
+    @Size(min = 1, max = 10, message = "Le nom de l'unité doit être entre 1 et 100 caractères")
     private String name;
+
+    @NotNull(message = "L'abréviation de l'unité ne peut pas être nulle")
+    @Size(min = 1, max = 10, message = "L'abréviation de l'unité doit être entre 1 et 10 caractères")
     private String abbreviation;
 }
