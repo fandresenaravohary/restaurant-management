8b46b61ac7c276daa4f0013d5f3a1694a472a374
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-04-07 08:41:56 +0300

    refactor: refactored save method in UnitsService

--- src/main/java/com/restaurantManagement/service/UnitServiceImpl.java
+++ src/main/java/com/restaurantManagement/service/UnitServiceImpl.java
@@ -3,19 +3,26 @@ package com.restaurantManagement.service;
 import com.restaurantManagement.dto.UnitsDto;
 import com.restaurantManagement.models.Units;
 import com.restaurantManagement.repository.UnitsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UnitServiceImpl implements UnitService {
 
-    @Autowired
     private UnitsRepository unitsRepository;
 
     @Override
     public Units save(UnitsDto unitsDto) {
+        Units unit;
 
-        Units unit = new Units();
+        if (unitsDto.getId() != null) {
+            unit = unitsRepository.findById(unitsDto.getId())
+                    .orElseThrow(() -> new IllegalArgumentException("Unit not found with ID: " + unitsDto.getId()));
+
+        } else {
+            unit = new Units();
+        }
         unit.setName(unitsDto.getName());
         unit.setAbbreviation(unitsDto.getAbbreviation());
 
