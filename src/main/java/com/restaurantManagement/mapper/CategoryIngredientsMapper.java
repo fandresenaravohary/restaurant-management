8b6ce66500b6f91dc31be3cb377824038d287c31
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-04-09 21:33:09 +0300

    feat: add CategoryIngredientsMapper

---
+++ src/main/java/com/restaurantManagement/mapper/CategoryIngredientsMapper.java
@@ -0,0 +1,34 @@
+package com.restaurantManagement.mapper;
+
+import com.restaurantManagement.dto.CategoryIngredientsDto;
+import com.restaurantManagement.dto.CategoryIngredientsSummarized;
+import com.restaurantManagement.models.CategoryIngredients;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryIngredientsMapper {
+
+    public CategoryIngredientsSummarized convertToCategoryIngredientsSummarized(CategoryIngredients categoryIngredients) {
+        if(categoryIngredients == null){
+            return null;
+        }
+        return CategoryIngredientsSummarized.builder()
+                .id(categoryIngredients.getId())
+                .name(categoryIngredients.getName())
+                .description(categoryIngredients.getDescription())
+                .createdAt(categoryIngredients.getCreatedAt())
+                .updatedAt(categoryIngredients.getUpdatedAt())
+                .build();
+    }
+
+    public CategoryIngredients convertToCategoryIngredients(CategoryIngredientsDto categoryIngredientsDto) {
+        if(categoryIngredientsDto == null){
+            return null;
+        }
+        return CategoryIngredients.builder()
+                .id(categoryIngredientsDto.getId())
+                .name(categoryIngredientsDto.getName())
+                .description(categoryIngredientsDto.getDescription())
+                .build();
+    }
+}
