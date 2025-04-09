b56d293cf4fe8625b656e38eef2e7742929bee1f
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-04-09 20:16:51 +0300

    feat: add IngredientsController

---
+++ src/main/java/com/restaurantManagement/controllers/IngredientsController.java
@@ -0,0 +1,38 @@
+package com.restaurantManagement.controllers;
+
+import com.restaurantManagement.dto.IngredientsDto;
+import com.restaurantManagement.dto.IngredientsSummarized;
+import com.restaurantManagement.service.IngredientsService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ingredients")
+@RequiredArgsConstructor
+public class IngredientsController {
+
+    private final IngredientsService ingredientsService;
+
+    @PostMapping
+    public ResponseEntity<IngredientsSummarized> create(@Valid @RequestBody IngredientsDto ingredientsDto) {
+        IngredientsSummarized savedIngredient = ingredientsService.save(ingredientsDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedIngredient);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<IngredientsSummarized>> findAll() {
+        List<IngredientsSummarized> ingredients = ingredientsService.findAll();
+        return ResponseEntity.ok(ingredients);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        ingredientsService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
