3c77afa1bde76038a3dae0b68142fe4e11249456
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-04-09 15:54:06 +0300

    feat: add method findAllIngredientsSummarized in IngredientsRepository

---
+++ src/main/java/com/restaurantManagement/repository/IngredientsRepository.java
@@ -0,0 +1,30 @@
+package com.restaurantManagement.repository;
+
+import com.restaurantManagement.dto.IngredientsSummarized;
+import com.restaurantManagement.models.Ingredients;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface IngredientsRepository extends JpaRepository<Ingredients, Long> {
+
+    @Query("""
+        SELECT new com.restaurantManagement.dto.IngredientsSummarized(
+            i.id,
+            i.name,
+            i.url,
+            u.name,
+            c.name,
+            s.id,
+            s.quantity,
+            i.createdAt,
+            i.updatedAt
+        )
+        FROM Ingredients i
+        LEFT JOIN i.units u
+        LEFT JOIN i.categoryIngredients c
+        LEFT JOIN i.stock s
+        """)
+    List<IngredientsSummarized> findAllIngredientsSummarized();
+}
